5aaf96c1530f563e7304d7fb970cedc2bcff5632
RosbergLiu <captainlmy@gmail.com>
2018-10-16 22:34:34 -0400

    Initial commit

---
+++ app/src/main/java/com/rosberg/project_from_hmc/MainActivity.java
@@ -0,0 +1,95 @@
+package com.rosberg.project_from_hmc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.loopj.android.http.*;
+import android.view.View;
+import java.util.ArrayList;
+import android.content.Intent;
+import android.speech.RecognizerIntent;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+import cz.msebera.android.httpclient.Header;
+
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private static final int VOICE_RECOGNITION_REQUEST_CODE = 1234;
+    private TextView recogresult;
+    private Button button;
+    private TextView resultstatu;
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recogresult = findViewById(R.id.textView);
+        resultstatu = findViewById(R.id.textView2);
+
+
+        button = findViewById(R.id.button);
+        button.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startVoiceRecognitionActivity();
+            }
+        });
+    }
+    private void startVoiceRecognitionActivity() {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_MAX_RESULTS, 1);
+        startActivityForResult(intent, VOICE_RECOGNITION_REQUEST_CODE);
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == VOICE_RECOGNITION_REQUEST_CODE && resultCode == RESULT_OK) {
+            ArrayList<String> result = data.getStringArrayListExtra(
+                    RecognizerIntent.EXTRA_RESULTS);
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.append(result.get(0));
+            recogresult.setText(stringBuilder);
+            String textweget = stringBuilder.toString();
+            String url = "http://160.39.239.137/";
+            String result_get = requestpost(url,textweget);
+            StringBuilder resultrepo = new StringBuilder();
+            resultrepo.append(result_get);
+            resultstatu.setText(resultrepo);
+
+
+
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private String requestpost(String url, String content){
+        AsyncHttpClient client = new AsyncHttpClient();
+        RequestParams params = new RequestParams();
+        params.put("text",content);
+        final StringBuilder resultrepo = new StringBuilder();
+        client.post(url, params, new TextHttpResponseHandler() {
+            @Override
+            public void onFailure(int i, Header[] headers, String s, Throwable throwable) {
+                Toast.makeText(getApplicationContext(),s,Toast.LENGTH_LONG).show();
+            }
+            @Override
+            public void onSuccess(int i, Header[] headers, String s) {
+                Toast.makeText(getApplicationContext(),s,Toast.LENGTH_LONG).show();
+            }
+        });
+        return resultrepo.toString();
+
+
+    }
+
+}
